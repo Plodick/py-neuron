ad18b8f705d991b76a3c0629e7b04f04b8cb04a5
Dmitry Plodukhin <dploduhin96@gmail.com>
2020-02-04 10:35:10 +0300

    Стадия обучения (простой пример)

--- exec/main.py
+++ exec/main.py
@@ -1,7 +1,8 @@
+from random import uniform
+
 import numpy as np
 
-T = 1
-
+# Матрица эталонных образов
 standard_images_matrix = np.array([[-1, -1, -1],
                                    [-1, -1, 1],
                                    [-1, 1, -1],
@@ -11,7 +12,11 @@ standard_images_matrix = np.array([[-1, -1, -1],
                                    [1, 1, -1],
                                    [1, 1, 1]])
 
+# Параметр пороговой функции
+T = len(standard_images_matrix[0]) / 2
 
+
+# Пороговая функция активации
 def threshold_function(x):
     return {
         x <= 0: 0,
@@ -22,14 +27,29 @@ def threshold_function(x):
 
 class HammingNeuron:
     def __init__(self, images_matrix):
-        self.weights = np.zeros((len(images_matrix), len(images_matrix[0])))
-        for i in range(len(images_matrix)):
-            for j in range(len(images_matrix[i])):
+        # Число образов
+        self.k = len(images_matrix)
+        # Число признаков
+        self.m = len(images_matrix[0])
+        # Матрица весовых коэффициентов нейронов первого слоя
+        self.weights = np.zeros((self.k, self.m))
+        for i in range(self.k):
+            for j in range(self.m):
                 self.weights[i][j] = images_matrix[i][j] / 2
+        # Задание обратных нейронов второго слоя
+        eps = uniform(0, 1 / self.k)
+        self.eps_matrix = np.zeros((self.k, self.k))
+        for i in range(self.k):
+            for j in range(self.k):
+                self.eps_matrix[i][j] = 1 if i == j else -eps
+        self.e_max = 0.1
 
 
 def main():
+    # Обучение
     neuron = HammingNeuron(standard_images_matrix)
+    # Использование
+
     print()
 
 
