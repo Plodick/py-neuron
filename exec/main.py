ba2c731068132417502f046ec6ec20e0daf7a9b9
Dmitry Plodukhin <dploduhin96@gmail.com>
2020-02-04 12:14:37 +0300

    Стадия использования (пример со статьи)

--- exec/main.py
+++ exec/main.py
@@ -1,6 +1,7 @@
 from random import uniform
 
 import numpy as np
+from numpy.linalg import norm
 
 # Матрица эталонных образов
 standard_images_matrix = np.array([[-1, -1, -1],
@@ -12,17 +13,10 @@ standard_images_matrix = np.array([[-1, -1, -1],
                                    [1, 1, -1],
                                    [1, 1, 1]])
 
-# Параметр пороговой функции
-T = len(standard_images_matrix[0]) / 2
-
-
-# Пороговая функция активации
-def threshold_function(x):
-    return {
-        x <= 0: 0,
-        0 < x <= T: x,
-        x > T: T
-    }[True]
+# Еще одна матрица
+standard_images_matrix_2 = np.array([[-1, -1, 1, -1, 1, -1, 1, -1, 1],
+                                     [-1, 1, -1, 1, 1, 1, -1, 1, -1],
+                                     [1, 1, 1, 1, -1, 1, 1, 1, 1]])
 
 
 class HammingNeuron:
@@ -31,12 +25,14 @@ class HammingNeuron:
         self.k = len(images_matrix)
         # Число признаков
         self.m = len(images_matrix[0])
+        # Параметр пороговой функции
+        self.t = self.m / 2
         # Матрица весовых коэффициентов нейронов первого слоя
         self.weights = np.zeros((self.k, self.m))
         for i in range(self.k):
             for j in range(self.m):
                 self.weights[i][j] = images_matrix[i][j] / 2
-        # Задание обратных нейронов второго слоя
+        # Матрица весов обратных связей
         eps = uniform(0, 1 / self.k)
         self.eps_matrix = np.zeros((self.k, self.k))
         for i in range(self.k):
@@ -44,13 +40,37 @@ class HammingNeuron:
                 self.eps_matrix[i][j] = 1 if i == j else -eps
         self.e_max = 0.1
 
+    # Пороговая функция активации
+    def threshold_function(self, x):
+        for i in range(len(x)):
+            x[i] = {
+                x[i] <= 0: 0,
+                0 < x[i] <= self.t: x[i],
+                x[i] > self.t: self.t
+            }[True]
+        return x
+
+    def feed_forward(self, x):
+        result = np.dot(self.weights, x) + self.t
+        return self.threshold_function(result)
+
+    def recalculate_second_layer(self, y):
+        result = np.dot(self.eps_matrix, y)
+        return self.threshold_function(result)
+
 
 def main():
     # Обучение
-    neuron = HammingNeuron(standard_images_matrix)
+    neuron = HammingNeuron(standard_images_matrix_2)
     # Использование
+    x = np.array([1, -1, -1, -1, 1, -1, 1, -1, 1])
+    y = neuron.feed_forward(x)
+    y_next = neuron.recalculate_second_layer(y)
 
-    print()
+    while norm(y_next - y) ** 2 > neuron.e_max:
+        y = y_next
+        y_next = neuron.recalculate_second_layer(y)
+    print(y_next)
 
 
 if __name__ == '__main__':
